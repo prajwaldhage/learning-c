627d677dc17042709c8ef64b82914aa3dc8e2cfb
Prajwal <psdhage1@gmail.com>
2025-12-22 23:34:00 +0530

    Day 4: Added Standard IO Functions

---
+++ 00_Introduction/standardio.c
@@ -0,0 +1,17 @@
+// Day 4: Standard IO Functions Program
+// Date: 22-12-2025
+
+#include <stdio.h>
+int main()
+{
+    int length, breadth, area;
+    printf("Enter Length: ");
+    scanf("%d", &length);
+    printf("Enter Breadth: ");
+    scanf("%d", &breadth);
+    area = length * breadth;
+    printf("Area: %d", area);
+}
+
+// Learnt About standard IO Functions
+// printf() and scanf()
